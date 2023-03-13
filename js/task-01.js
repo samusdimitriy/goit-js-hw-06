b0e6a685ab63d4f84c6adc93539f536771f35bd0
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2023-03-13 13:16:52 +0100

    task 1 done

---
+++ js/task-01.js
@@ -0,0 +1,7 @@
+const countCategories = document.querySelectorAll(".item");
+console.log(`Number of categories: ${countCategories.length}`);
+
+countCategories.forEach((element) => {
+  console.log(`Category: ${element.firstElementChild.textContent}`);
+  console.log(`Elements: ${element.lastElementChild.children.length}`);
+});

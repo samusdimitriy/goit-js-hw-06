37bbe3ceaa51e6932c53149fc858f64dae081cee
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2023-03-14 14:53:36 +0100

    task 3 done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,15 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+ingredients.map((ingredient) => {
+  const li = document.createElement("li");
+  li.textContent = ingredient;
+  li.classList.add("item");
+  document.querySelector("#ingredients").append(li);
+});
--- js/task-03.js
+++ js/task-03.js
@@ -1,14 +1,35 @@
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Белый и черный длинношерстный кот",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Оранжево-белая карповая рыба возле желтой карповой рыбы",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Группа бегущих лошадей",
   },
 ];
+
+const createStringImages = ({ url, alt }) => {
+  return `<li><img src="${url}" alt="${alt}"></li>`;
+};
+
+const galleryEl = images.map(createStringImages).join("");
+console.log(galleryEl);
+
+const gallery = document.querySelector(".gallery");
+gallery.insertAdjacentHTML("afterbegin", galleryEl);
+
+gallery.style.display = "flex";
+gallery.style.listStyle = "none";
+gallery.style.justifyContent = "space-around";
+
+const imagesEl = document.querySelectorAll("img");
+console.log(imagesEl);
+imagesEl.forEach((image) => {
+  image.style.width = "300px";
+  image.style.height = "200px";
+});
